7f7660e40f82f4c28370fd679969aaf161428686
Ysweb3 <yssh88972@gmail.com>
2024-05-16 22:24:35 +0530

    Add canvas size slider and reset button

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
-const container = document.getElementById("container");
-const rowContainer = document.getElementById("row-container");
+const container = document.querySelector("#container");
+const rowContainer = document.querySelector("#row-container");
 const cell = document.getElementById("cell");
+const sizeSlider = document.getElementById("grid-size");
+const sizeSliderOutput = document.getElementById("slider-value");
+const sizeBtn = document.getElementById("Change-Btn");
+const resetBTn = document.getElementById("Reset-Btn");
+const allCells = document.getElementById("#cell");
+const canvasSize = 800;
+let gridValue;
+let color = "Black";
 
 
-function createGrid(a) {
+function CreateGrid(a) {
+    console.log(a);
     for (i = 0; i < (a); i++){
-        let newrowContainer = document.createElement("div");
-        container.appendChild(newrowContainer).id = "row-container"
+        let newRowContainer = document.createElement("div");
+        container.appendChild(newRowContainer).id = "row-container";
         for (let j = 0; j < a; j++) {
             let cell = document.createElement("div");
-            newrowContainer.appendChild(cell).id = "cell";      
+            newRowContainer.appendChild(cell).id = "cell";    
+            cell.style.height =`${canvasSize / a + 0.01}px`;
+            cell.style.width =`${canvasSize / a + 0.01}px` ;
         }
     };
-
 }
 
-function createColumn(colNum){
-
-
-}
-createGrid(26);
-
-cell.addEventListener("hover",() => {
-    console.log("HELLO")
-    cell.style.backgroundColor = "red";
+function GridColor(color){
+    
+    const allCells = document.querySelectorAll("#cell");
+    allCells.forEach(box => {
+    box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = color; 
 });
+  });
+}
+
+function ClearCanvas(){
+    container.innerHTML= "";
+}
+
+function CreateCanvas(size){
+    ClearCanvas();
+    CreateGrid(size);
+    GridColor(color)
+    
+}
+function Slidervalue(){
+    sizeSlider.onchange = (e) => {
+    let vlaue = e.target.value;
+    CreateCanvas(vlaue);
+    sizeSliderOutput.textContent = sizeSlider.value;
+    }
+    return value;
+}   
+
+
+
+resetBTn.addEventListener("click", () => {
+    CreateCanvas(value)
+    
+});
+
+value = Slidervalue()
+
+
+
+         
+
